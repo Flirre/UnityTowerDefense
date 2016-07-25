e71e096aae7b4d6098b45cb1c419643e61c1a227
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-25 13:29:30 +0200

    Added path code so the enemies follow a set path

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,33 +6,51 @@ public class Enemy : MonoBehaviour {
     GameObject walkPath;
     Transform targetNode;
     int nodeIndex = 0;
+    float speed = 100;
 
 	// Use this for initialization
 	void Start () {
         walkPath = GameObject.Find("WalkPath");
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(targetNode == null)
         {
-            getNextNode();
+            GetNextNode();
             if(targetNode == null)
             {
                 //if(noCollide(otherEnemy))
-                baseAttack();
+                //Attack(Base);
             }
+        }
+
+        Vector3 direction = targetNode.position - this.transform.localPosition;
+
+        float rangeThisFrame = speed * Time.deltaTime;
+
+        if(direction.magnitude <= rangeThisFrame)
+        {
+            //node reached
+            targetNode = null;
+
+        }
+
+        else
+        {//move to targetNode
+            transform.Translate(direction.normalized * rangeThisFrame, Space.World);
+            this.transform.rotation = Quaternion.LookRotation(direction); //makes the enemy look toward node they are approaching.
+
         }
 	
 	}
 
-    void getNextNode() {
+    void GetNextNode() {
         targetNode = walkPath.transform.GetChild(nodeIndex);
         nodeIndex++;
     }
 
-    void baseAttack()
+    void Attack()
     {
 
     }
