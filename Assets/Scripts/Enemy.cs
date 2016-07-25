a93f045e999c6f79e3d9c333bef28050bcaf79f0
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-25 11:34:54 +0200

    created some scripts and started writing in Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    GameObject walkPath;
+    Transform targetNode;
+    int nodeIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        walkPath = GameObject.Find("WalkPath");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(targetNode == null)
+        {
+            getNextNode();
+            if(targetNode == null)
+            {
+                //if(noCollide(otherEnemy))
+                baseAttack();
+            }
+        }
+	
+	}
+
+    void getNextNode() {
+        targetNode = walkPath.transform.GetChild(nodeIndex);
+        nodeIndex++;
+    }
+
+    void baseAttack()
+    {
+
+    }
+}
