56f655b1fab0c70357684b917cf1b71b2cc375a3
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-25 22:21:19 +0200

    A collision script for aiming at enemies.

---
+++ Assets/Scripts/TowerCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerCollision : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        print("Collision detected with trigger object " + other.name);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        print("Still colliding with trigger object " + other.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+    }
+
+
+
+}
+
+
+// Collisions found in this tutorial https://www.binpress.com/tutorial/unity-3d-collisions-basics/114
