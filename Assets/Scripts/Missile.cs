99a1dd005b4ca6b2289335a64feeccaf5099699f
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-25 22:20:26 +0200

    Added Missile with basic thoughts for solution.

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour
+{
+    //Enemy enemy
+    void FixedUpdate()
+    {
+        //check for closest target.
+        //Lock on and follow til impact.
+    }
+
+}
