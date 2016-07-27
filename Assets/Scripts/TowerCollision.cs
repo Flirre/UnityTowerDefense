aa92dea0af263d3af643c393efe15cacf91d55fa
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-27 19:17:16 +0200

    Latest version of TowerCollision.cs

--- Assets/Scripts/TowerCollision.cs
+++ Assets/Scripts/TowerCollision.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class TowerCollision : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
+        if(other.tag == "Enemy")
         print("Collision detected with trigger object " + other.name);
     }
 
     void OnTriggerStay(Collider other)
     {
+        if (other.tag == "Enemy")
         print("Still colliding with trigger object " + other.name);
     }
 
     void OnTriggerExit(Collider other)
     {
+        if (other.tag == "Enemy")
         print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
     }
 
