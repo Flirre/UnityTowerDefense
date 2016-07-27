8ec563d171b62d20e214c0c6c30506fada72d8aa
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-27 19:16:00 +0200

    An update to missile.cs

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -3,11 +3,37 @@ using System.Collections;
 
 public class Missile : MonoBehaviour
 {
-    //Enemy enemy
+    GameObject target = null;
     void FixedUpdate()
     {
-        //check for closest target.
+        if(target == null)
+        {
+            target = getClosestEnemy();
+        }
         //Lock on and follow til impact.
+    }
+
+    GameObject getClosestEnemy()
+    {
+        GameObject[] enemies;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        float dist = Mathf.Infinity;
+        Vector3 pos = transform.position;
+        foreach(GameObject e in enemies)
+        {
+            Vector3 diff = e.transform.position - pos;
+            float currentDist = diff.sqrMagnitude;
+            if(currentDist < dist)
+            {
+                target = e;
+                dist = currentDist;
+            }
+
+        }
+
+
+        return target;
     }
 
 }
