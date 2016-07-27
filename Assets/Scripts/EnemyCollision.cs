b6cad3c797358ef5f0f5cfd8ad4d05c4fe7fbb40
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-27 19:15:52 +0200

    script for handling enemy collision with missiles

---
+++ Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollision : MonoBehaviour
+{
+
+    
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Enemy e = gameObject.GetComponent<Enemy>();
+        print(e.health);
+        e.health = e.health - 10;
+        print(e.health);
+        if(e.health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        //print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+    }
+}
