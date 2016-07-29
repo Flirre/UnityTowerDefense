69d59bb5fc1648c8a2635d3d81f04fe8d3b7cb57
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-29 14:24:00 +0200

    Placementpads for Towers for fixed position.

---
+++ Assets/Scripts/TowerPad.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerPad : MonoBehaviour {
+
+    bool pressed = false;
+    bool towerOnPad = false;
+
+    void Update()
+    {
+
+        if ((pressed == true) & (towerOnPad == false))
+        {
+            Debug.Log("PRESSED");
+            Instantiate(Resources.Load("Tower"), transform.position, Quaternion.identity);
+            towerOnPad = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("DOWN");
+        pressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("UP");
+        pressed = false;
+    }
+
+}
