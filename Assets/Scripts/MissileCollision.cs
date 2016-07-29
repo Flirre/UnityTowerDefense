3cf017df69b6e63ae4c45b36c5af59aa8ac4c2c1
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-29 14:23:17 +0200

    Missiles are removed if no target. Or crash.

---
+++ Assets/Scripts/MissileCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        print("BEFORE IF");
+        if(collisionInfo.collider.tag == "Enemy")
+        {
+            print("IN IF");
+            Destroy(gameObject);
+        }
+        print("AFTER IF");
+        //print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        //print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+    }
+}
