4b028173a6d6d2e89ef17eae167584b99557f529
Gysztaf@gmail.com <gysztaf@gmail.com>
2016-07-29 14:21:45 +0200

    Added scripts for the base and how it collides.

---
+++ Assets/Scripts/BaseCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Base b = gameObject.GetComponent<Base>();
+        print(b.health);
+        b.health = b.health - 10;
+        print(b.health);
+        if (b.health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        //print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+    }
+}
